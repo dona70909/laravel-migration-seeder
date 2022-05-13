7b133811baf3d0d147faa1db5d54b95512b791ee
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 14:35:10 +0200

    Style main_trains

--- resources/views/partials/main/main_trains.blade.php
+++ resources/views/partials/main/main_trains.blade.php
@@ -4,7 +4,7 @@
             <ul class="list-unstyled">
                 @foreach ($trains as $train)
                     <li class="border-top p-3">
-                        <h5>Partenza :{{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</h5>
+                        <h5>Partenza: {{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</h5>
                         <h5>Arrivo: {{$train->data_di_arrivo}} - {{$train->stazione_di_arrivo}}</h5>
                     </li>
                 @endforeach 
