28309182148c45bd11e41c618f3c7e0436f6d770
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 13:41:40 +0200

    Set views and routes

---
+++ resources/views/guest/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('head-title','Home')
+    
+
+@section('header-content')
+    @include('partials.header.header_nav')    
+@endsection
+
+@section('main-content')
+    @include('partials.main.main_trains')    
+@endsection
