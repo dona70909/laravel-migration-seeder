632b5bcf754b47bf288625c907f5ad499f455611
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 14:37:55 +0200

    style train data and stazione

--- resources/views/partials/main/main_trains.blade.php
+++ resources/views/partials/main/main_trains.blade.php
@@ -4,8 +4,8 @@
             <ul class="list-unstyled">
                 @foreach ($trains as $train)
                     <li class="border-top p-3">
-                        <h5>Partenza: {{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</h5>
-                        <h5>Arrivo: {{$train->data_di_arrivo}} - {{$train->stazione_di_arrivo}}</h5>
+                        <h5>Partenza: <strong class="text-uppercase">{{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</strong></h5>
+                        <h5>Arrivo: <strong class="text-uppercase">{{$train->data_di_arrivo}} - {{$train->stazione_di_arrivo}}</strong></h5>
                         <h4 class="text-uppercase">{{$train->azienda}}</h4>
                     </li>
                 @endforeach 
