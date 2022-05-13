fc858e555243ab3496411ba1788d2ecf35111170
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 14:25:05 +0200

    style header

--- resources/views/partials/header/header_nav.blade.php
+++ resources/views/partials/header/header_nav.blade.php
@@ -1,7 +1,7 @@
 <section class="container-fluid">
-    <div class="row">
+    <div class="row px-3 py-2">
         <div class="col-12">
-            header-nav
+            <h1 ><a class="text-decoration-none text-dark" href="{{url('/')}}">Home</a></h1>
         </div>
     </div>
 </section>
