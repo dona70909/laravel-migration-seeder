bc6b2415a93a9aaf328012ff6dd42f4af5f59e0a
Donatella7 <campaniellodonatella@gmail.com>
2022-05-16 16:40:12 +0200

    Installed bootsrap and icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
     {{-- css --}}
-    <link rel="stylesheet" href="{{asset("css/base.css")}}"> 
+    <link rel="stylesheet" href="{{asset("css/app.css")}}"> 
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Trains - @yield('head-title')</title>
 </head>
 <body>
--- resources/views/partials/main/main_trains.blade.php
+++ resources/views/partials/main/main_trains.blade.php
@@ -1,12 +1,15 @@
 <section class="container-fluid">
     <div class="row p-5">
-        <div class="col-12">
+        <div class="col-6">
             <ul class="list-unstyled">
                 @foreach ($trains as $train)
-                    <li class="border-top p-3">
-                        <h5>Partenza: <strong class="text-uppercase">{{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</strong></h5>
-                        <h5>Arrivo: <strong class="text-uppercase">{{$train->data_di_arrivo}} - {{$train->stazione_di_arrivo}}</strong></h5>
-                        <h4 class="text-uppercase">{{$train->azienda}}</h4>
+                    <li class="border-top p-3 d-flex ">
+                        <div class="departure-box">
+                            <h6>Partenza: <strong>{{$train->stazione_di_partenza}}</strong> <i class="bi bi-arrow-right-short mx-2"></i> </h6>
+                        </div>
+                        <div class="arrival-box">
+                            <h6>Arrivo: <strong >{{$train->stazione_di_arrivo}}</strong></h6>
+                        </div>
                     </li>
                 @endforeach 
             </ul>
@@ -16,3 +19,8 @@
         </div>
     </div>
 </section>
+
+
+{{--      <h5>Partenza: <strong class="text-uppercase">{{$train->data_di_partenza}} - {{$train->stazione_di_partenza}}</strong></h5>
+                        <h5>Arrivo: <strong class="text-uppercase">{{$train->data_di_arrivo}} - {{$train->stazione_di_arrivo}}</strong></h5>
+                        <h4 class="text-uppercase">{{$train->azienda}}</h4> --}}
